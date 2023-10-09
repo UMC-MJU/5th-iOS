fa048307a9e9d7c4145f5f08f9c8682f1bfddfc8
bae-suho <115385697+bae-suho@users.noreply.github.com>
2023-10-09 22:50:33 +0900

    Add files via upload

---
+++ suho_workspace/BaeMin_Transition/BaeMin_Transition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BaeMin_Transition
+//
+//  Created by 배수호 on 10/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ suho_workspace/BaeMin_Transition/BaeMin_TransitionUITests/BaeMin_TransitionUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BaeMin_TransitionUITestsLaunchTests.swift
+//  BaeMin_TransitionUITests
+//
+//  Created by 배수호 on 10/9/23.
+//
+
+import XCTest
+
+final class BaeMin_TransitionUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

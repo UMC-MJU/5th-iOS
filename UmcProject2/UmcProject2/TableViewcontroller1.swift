f9dd1474faa1dcd4066407bf5632142950940597
choi-yuna <mjung0226@naver.com>
2023-10-10 16:23:25 +0900

    3주차 미션

---
+++ UmcProject2/UmcProject2/TableViewcontroller1.swift
@@ -0,0 +1,12 @@
+//
+//  TableViewcontroller1.swift
+//  UmcProject2
+//
+//  Created by 최윤아 on 2023/10/09.
+//
+
+import UIKit
+
+class TableViewcontroller1: NSObject {
+
+}
---
+++ UmcProject2/UmcProject2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UmcProject2
+//
+//  Created by 최윤아 on 2023/10/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

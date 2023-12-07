a8637795241b29a7590d6fb277f8e04d6aa04597
dogyungkim <prunsoli12@gmail.com>
2023-12-07 12:10:19 -0600

    9&10주차

---
+++ 브라이언_김도경/10주차 미션/OpenWeahter(SwiftUI)/OpenWeahter(SwiftUI)/OpenWeahter_SwiftUI_App.swift
@@ -0,0 +1,22 @@
+//
+//  OpenWeahter_SwiftUI_App.swift
+//  OpenWeahter(SwiftUI)
+//
+//  Created by 김도경 on 12/7/23.
+//
+
+import SwiftUI
+
+
+
+@main
+struct OpenWeahter_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+
+
---
+++ 브라이언_김도경/9주차 미션/LOGIN(SwiftUI)/LOGIN(SwiftUI)/LOGIN_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  LOGIN_SwiftUI_App.swift
+//  LOGIN(SwiftUI)
+//
+//  Created by 김도경 on 12/3/23.
+//
+
+import SwiftUI
+
+@main
+struct LOGIN_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
